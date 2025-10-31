82b70c8328c75380d67dac61c3c9b5c09ecab2c9
VadoVates <marekgor@gmail.com>
2025-10-31 20:55:12 +0100

    init commit

---
+++ genetic_algorithm.py
@@ -0,0 +1,23 @@
+def euc_2d(result:float):
+    try:
+        if result > 0:
+            integer_result = int (result)
+            if result - integer_result < 0.5:
+                return integer_result
+            else:
+                return integer_result+1
+        else:
+            print ("ODLEGŁOŚĆ <= 0, nieobsługiwany wyjątek")
+            return -1
+    except Exception as e:
+        print (f"Nieprawidłowe dane wejściowe, treść błędu: {e}")
+        return -1
+    
+def att(x1:int, y1:int, x2:int, y2:int):
+    xd = x1 - x2
+    yd = y1 - y2
+    r = ((xd*xd - yd*yd) / 10.0) ** 0.5
+    t = euc_2d(r)
+    return t
+ 
+print (euc_2d(0))
